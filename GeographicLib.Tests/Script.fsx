d6eac174654c9e26ea64ee7305728d8d3c286477
Rob Lyndon <rob.lyndon@gmail.com>
2016-05-06 16:41:50 +0300

    Added a script to the testing project to show a simple way to calculate distances.

--- GeographicLib.Tests/Script.fsx
+++ GeographicLib.Tests/Script.fsx
@@ -1,6 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "PortableLibrary1.fs"
-open GeographicLib.Tests
+#r "bin/Release/GeographicLib.dll"
+
+open GeographicLib
+
+let geo = Geodesic.WGS84
+let landsEndLocation = new GeodesicLocation(50.0657<deg>, -5.7113<deg>)
+let johnOGroatsLocation = new GeodesicLocation(58.6373<deg>, 3.0689<deg>)
+let distance = geo.Distance landsEndLocation johnOGroatsLocation
+
+
+
 
